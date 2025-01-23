a3cd62d16ccb029f590a407581c4f65676467e57
Evelyn Kim <ekim25@students.polytechnic.org>
2025-01-23 12:22:11 -0800

    From PlaylistProject file

---
+++ Song.java
@@ -0,0 +1,31 @@
+package PlaylistProject;
+/**
+ * File for a Song class to be used in the Playlist Project
+ * @author
+ * @version
+ */
+public class Song {
+    //Fields-- what information do we want each Song to store?
+
+
+
+
+    /**
+     * Constructor-- what information needs to be given to make a Song?
+     * How will you handle 'liked' songs? It makes sense for a Song not be 'liked' by default
+     */
+
+
+
+
+
+     /**
+      * Methods-- what will you want each Song to do?
+      * Consider all the getter methods (getName, getArtist, etc.)
+      * You should probably have a toString method to be able to get the information for the full
+      * song easily as well!
+      * What kind of mutator (setter) methods will you need?
+      */
+
+
+}
